2058682879628f90411f8e293ceeb7784fda1afe
Juanse1331 <juansebastian.suarez13@gmail.com>
2026-05-22 10:49:15 -0500

    fix: usar railway.toml para forzar start.sh como comando de inicio

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "==> Running migrations..."
 python backend/manage.py migrate --noinput
+echo "==> Running collectstatic..."
 python backend/manage.py collectstatic --noinput
+echo "==> Starting gunicorn..."
 exec gunicorn config.wsgi:application --chdir backend --bind 0.0.0.0:$PORT
